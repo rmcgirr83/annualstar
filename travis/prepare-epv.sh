24a8895ff08ac45727baa9bf3fb43444a64ec6d7
Richard McGirr <rmcgirr83@gmail.com>
2020-11-03 07:12:44 -0600

    Update prepare-epv again

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -17,7 +17,7 @@ NOTESTS=$2
 if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
 then
 	cd phpBB
-	composer --no-update-with-dependencies --dev --no-interaction
+	composer --update-with-dependencies --dev --no-interaction
 	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
 	cd ../
 fi
