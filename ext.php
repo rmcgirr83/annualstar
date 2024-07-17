c659edb99079f33f1a1c7a6272d6612acff194ff
Rich McGirr <rmcgirr83@gmail.com>
2024-07-17 05:15:18 -0500

    Fix actions

--- ext.php
+++ ext.php
@@ -25,6 +25,6 @@ class ext extends \phpbb\extension\base
 	public function is_enableable()
 	{
 		$config = $this->container->get('config');
-		return phpbb_version_compare($config['version'], '3.2', '>=');
+		return phpbb_version_compare($config['version'], '3.3', '>=');
 	}
 }
